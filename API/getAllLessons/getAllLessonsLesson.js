69f8ec91d11d48871db38dba9fcd33908551945f
ieser <59726923+ieser@users.noreply.github.com>
2024-06-05 18:16:50 +0200

    Update getAllLessonsLesson.js

--- API/getAllLessons/getAllLessonsLesson.js
+++ API/getAllLessons/getAllLessonsLesson.js
@@ -1,1 +1,11 @@
 
+const mongoose = require('mongoose');
+
+const lesson_schema = new mongoose.Schema({
+    id: Number,
+    title: String,
+    link: String,
+    content: String
+}, { collection: 'tedx_data' });
+
+module.exports = mongoose.model('lesson', lesson_schema);
