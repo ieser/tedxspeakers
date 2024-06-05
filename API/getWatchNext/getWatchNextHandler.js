7f4bf79e5564e3bba15d97714a7af7c1673342e6
ieser <59726923+ieser@users.noreply.github.com>
2024-06-05 17:47:51 +0200

    Update and rename watchNext.js to getWatchNextHandler.js

---
+++ API/getWatchNext/getWatchNextHandler.js
@@ -0,0 +1,41 @@
+const connect_to_db = require('./db');
+
+// GET WATCH NEXT
+
+const talk = require('./Talk');
+
+module.exports.get_watch_next = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    let body = {}
+    if (event.body) {
+        body = JSON.parse(event.body)
+    }
+    if(!body.slug) {
+        callback(null, {
+            statusCode: 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not fetch the talks. Slug is null.'
+        })
+    }
+
+    
+    connect_to_db().then(() => {
+        talk.find({"slug": body.slug})
+            .then(talks => {
+                var related = talks[0].related
+                    callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify(related)
+                    })
+                }
+            )
+            .catch(err =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the talks.'
+                })
+            );
+    });
+};
